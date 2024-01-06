ca433ce8f3de6fe8728c868bd16fbd36c72aca1b
saed <saed.madkaash@gmail.com>
2024-01-06 06:23:53 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    ret = ""
-    for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'C'):
-            ret += my_string[i]
-    return ret 
+    dict = {ord(char): None for char in "cC"}
+    new_string = my_string.translate(dict)
+    return new_string
