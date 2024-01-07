d063584d7e266aec5966261304b90f0ff646cd3c
saed <saed.madkaash@gmail.com>
2024-01-06 22:53:08 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    str_len, first_char = len(sentence), sentence[0]
-    return (str_len, first_char)
+    return (len(sentence),sentence[0] if len(sentence) > 0 else None)
