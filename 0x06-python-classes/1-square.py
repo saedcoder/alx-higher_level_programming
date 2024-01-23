a998702d347c5bd8a826282dbcb853691a34fc05
saed <saed.madkaash@gmail.com>
2024-01-23 08:14:56 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 class Square:
-    """Square class"""
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    """
 
+    
     def __init__(self, size):
+        """Initializes the data."""
         self.__size = size
