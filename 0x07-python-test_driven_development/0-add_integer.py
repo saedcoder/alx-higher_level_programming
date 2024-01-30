038744fdb209b2a67574064c4c95f5d8987d4f2f
saed <saed.madkaash@gmail.com>
2024-01-30 00:19:43 -0800

    updated files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-Module to sum
-add_integer: adds two integers and return
-"""
+add_integer = __import__('0-add_integer').add_integer
 
-
-def add_integer(a, b=98):
-    """Return sum of a + b
-    Args: a = int/float b = int/float
-    """
-
-    if isinstance(a, (int, float)) is False:
-        raise TypeError('a must be an integer')
-    if isinstance(b, (int, float)) is False:
-        raise TypeError('b must be an integer')
-    return int(a + b)
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
